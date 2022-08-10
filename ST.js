7ec0c7de55e9d1ccf5717fa37da64580ca042288
Harshpreet Singh <harshpreet1410.be20@chitkara.edu.in>
2022-08-11 02:52:34 +0530

    Made the website more functional and added more pages to it with the blog creation form aswell.

--- ST.js
+++ ST.js
@@ -1,12 +1,40 @@
 function getPhotos() {
+    $("#getPhot").hide();   
     console.log("It is Working");
     $.get('https://jsonplaceholder.typicode.com/photos',function(data){
         for(let i=0;i<data.length;i++){
             var photo = data[i].url;
-            $("section").append(`<img src="${photo}"/>`);
+            $("#images").append(`<img src="${photo}"/>`);
         }
     }).fail(function(xhr,textStatus,errorThrown){
         console.log("Error: " + errorThrown)
     })
 }
-$("#getpho").click(getPhotos());
+$("#blog").hide();
+$("#form").hide();
+$("#blogbtn").click(function() {
+    $("#blog").show();
+    $("#images").hide();
+    $("#form").hide();
+})
+$("#homebtn").click(function() {
+    $("#images").show();
+    $("#blog").hide();
+    
+    $("#form").hide();
+})
+$("#forumbtn").click(function() {
+    $("#form").show();
+    $("#blog").hide();
+    $("#images").hide();
+    
+})
+$("#blogsubmit").click(function() {
+    var btit=$("#tiform").val();
+    var bcont=$("#conform").val();
+    $("#blog").show();
+    $("#form").hide();
+    $("#blog").append(`<div id="blogbody"><div id="blogtitle">${btit}</div><div id="blogcontent">${bcont}</div></div>`)
+
+});
+$("#getpho").click(getPhotos);
